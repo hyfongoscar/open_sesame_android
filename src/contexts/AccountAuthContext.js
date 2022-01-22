62a01e608c2b7e76f6a622df312e0a46c2343b64
PotatoHY <oscarmaths9141@gmail.com>
2022-01-22 18:56:29 +0800

    minor repositioning

--- src/contexts/AccountAuthContext.js
+++ src/contexts/AccountAuthContext.js
@@ -8,52 +8,55 @@ const AccountAuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  const login = async (email, password) => {
+    let obj = {}
+    await auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(userCredential => {
+        obj.userCredential = userCredential
+        setUser(userCredential.user)
+      })
+      .catch(error => {
+        obj.errorCode = error.code;
+        obj.errorMessage = error.message;
+      });
+    return obj
+  }
+
+  const register = async (displayName, email, password) => {
+    firestore()
+        .collection('username')
+        .doc(email)
+        .set({
+          name: displayName,
+        })
+        .then(() => {
+          console.log('username registered');
+        });
+    let obj = {}
+    await auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(userCredential => {
+        obj.userCredential = userCredential
+      })
+      .catch(error => {
+        obj.errorCode = error.code;
+        obj.errorMessage = error.message;
+      });
+    return obj
+  }
+
+  const logout = async () => {
+    await auth()
+      .signOut()
+      .then(() => console.log('User signed out!'));
+  }
+
   return (
       <AccountAuthContext.Provider
           value={{
-            user,
-            loading,
-            login: async (email, password) => {
-              let obj = {}
-              await auth()
-                .signInWithEmailAndPassword(email, password)
-                .then(userCredential => {
-                  obj.userCredential = userCredential
-                  setUser(userCredential.user)
-                })
-                .catch(error => {
-                  obj.errorCode = error.code;
-                  obj.errorMessage = error.message;
-                });
-              return obj
-            },
-            register: async (displayName, email, password) => {
-              firestore()
-                  .collection('username')
-                  .doc(email)
-                  .set({
-                    name: displayName,
-                  })
-                  .then(() => {
-                    console.log('username registered');
-                  });
-              let obj = {}
-              await auth()
-                .createUserWithEmailAndPassword(email, password)
-                .then(userCredential => {
-                  obj.userCredential = userCredential
-                })
-                .catch(error => {
-                  obj.errorCode = error.code;
-                  obj.errorMessage = error.message;
-                });
-              return obj
-            },
-            logout: async () => {
-              await auth()
-                .signOut()
-                .then(() => console.log('User signed out!'));
-            },
+            user, loading,
+            login, register, logout
           }}
       >
         {children}
