4b389bb529739ab38a59e23ccacbc0f833f99d95
PotatoHY <oscarmaths9141@gmail.com>
2022-01-27 19:55:07 +0800

    Discard user ids in friend requests

--- src/screens/FriendRequestScreen.js
+++ src/screens/FriendRequestScreen.js
@@ -1,90 +1,100 @@
 import React, { useContext, useEffect, useLayoutEffect, useState } from 'react'
 import { Alert, StyleSheet, View, Text, TouchableOpacity, FlatList } from 'react-native'
 import { Button, Title, TextInput } from 'react-native-paper';
-import firestore, {arrayUnion} from '@react-native-firebase/firestore';
+
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 import { AccountAuthContext } from '../contexts/AccountAuthContext'
 
-
-
 export default function FriendRequestScreen({ navigation }){
-    const [email, setEmail] = useState('')
-    const {user} = useContext(AccountAuthContext);
-    const [info, setInfo] = useState();
+  const { user } = useContext(AccountAuthContext)
 
-    useLayoutEffect(() => {
-        const subscriber = firestore()
-          .collection('friendRequest')
-          .where('rid','==',1)
-          .where('confirmed','==',false)  
-         // .orderBy('createdAt','desc')
-          .onSnapshot(querySnapshot => {
-            setInfo(
-              querySnapshot.docs.map(doc => ({
-                s_email: doc.data().s_email,
-                r_email: doc.data().r_email,
-                rid: doc.data().rid,
-                sid: doc.data().sid,
-                createdAt: doc.data().createdAt,
-              }))
-            );
-          });
-         return subscriber;
+  const [email, setEmail] = useState('')
+  const [requestList, setRequestList] = useState();
+
+  useLayoutEffect(() => {
+    const subscriber = firestore()
+      .collection('friendRequest')
+      .where('r_email', '==', user.email)
+      .where('confirmed','==',false)  
+      // .orderBy('createdAt','desc')
+      .onSnapshot(querySnapshot => {
+        setRequestList(
+          querySnapshot.docs.map(doc => ({
+            s_email: doc.data().s_email,
+            r_email: doc.data().r_email,
+            createdAt: doc.data().createdAt,
+          }))
+        );
+      });
+      return subscriber;
+  })
+
+  const sendFriendRequest = async () => {
+    var emailExists
+    await auth().fetchSignInMethodsForEmail(email).then((results) => {
+      emailExists = (results.length > 0)
     })
 
-    const getID = (email) =>{
-        return 2;
-    }
+    const friendQuery = await firestore()
+      .collection('friendList')
+      .where('friend_email_pair', 'array-contains', email)
+      .get()
 
-    const sendFriendRequest = async () => {
-        const rEmail = await firestore()
-        .collection('username')
-        .doc(email) 
-        .get();
-        if(rEmail.exists)
-        {
-            firestore().collection('friendRequest').doc().set({
-                s_email: "hello@gmail.com",
-                r_email: email,
-                sid: 1,
-                rid: getID(email),
-                sender_id_pair: [1, getID(email)],
-                createdAt: new Date(),
-                confirmed: false,
-            });
-        }
-        else 
-            Alert.alert("Email Not Registered")
-    }
+    const sentQuery = await firestore()
+    .collection('friendRequest')
+    .where('s_email', '==', user.email)
+    .get()
 
-    const acceptFriendRequest = async (sid, rid, s_email, r_email) =>{
-        const q = await firestore()
-          .collection('friendRequest')
-          .where('sid','==',sid)
-          .where('rid','==',1)
-           .get();
-        q.docs[0].ref.update({confirmed: true});
-
-        firestore().collection('friendList').doc().set({
-            email_pair: [r_email, s_email], 
-            friend_id_pair: [sid, rid],
-        })
-      
+    if (email == user.email) 
+      Alert.alert("", "You cannot add yourself as friend.")
+    else if (friendQuery.docs.length > 0) 
+      Alert.alert("", "You have already added this user.")
+    else if (sentQuery.docs.length > 0) 
+      Alert.alert("", "You have already sent a request to this user.")
+    else if (emailExists) {
+      firestore().collection('friendRequest').doc().set({
+        s_email: user.email,
+        r_email: email,
+        createdAt: new Date(),
+        confirmed: false,
+      })
+      Alert.alert("Request sent!", "Please wait for them to accept your request")
     }
-    
-    return (
-        <View>
-            <TextInput
-                label="Enter Email Address"
-                numberOfLines={1}
-                onChangeText={(email) => setEmail(email)}
-            />
-            <Button
-                onPress={ () => {sendFriendRequest()}}
-            >Send Friend request</Button>
+    else 
+      Alert.alert("We cannot found this user", "Please check their email again")
+  }
+
+  const acceptFriendRequest = async (s_email, r_email) =>{
+    const query = await firestore()
+      .collection('friendRequest')
+      .where('s_email','==',s_email)
+      .where('r_email','==',r_email)
+      .get()
+    await query.docs[0].ref.update({confirmed: true})
+
+    await firestore().collection('friendList').doc().set({
+      friend_email_pair: [r_email, s_email]
+    })
+    Alert.alert("Success", s_email + " is now your friend!")
+  }
+  
+  return (
+    <View>
+      <TextInput
+        label="Your friend's email"
+        numberOfLines={1}
+        onChangeText={setEmail}
+      />
+      <Button
+        onPress={ () => {sendFriendRequest()}}
+      >Send Friend request</Button>
+
+      <Text>Pending requests:</Text>
       <FlatList
-        data ={info}
-        keyExtractor={item=>item.sid}
+        data ={requestList}
+        keyExtractor={request => request.s_email}
         renderItem={({item}) => (
           <TouchableOpacity 
             style = {styles.profile}
@@ -92,67 +102,67 @@ export default function FriendRequestScreen({ navigation }){
             <View style={styles.userInfo}>
               <View style={styles.textSection}>
                 <Text style = {styles.userName}> {item.s_email}</Text>
-                <Button onPress={() => {acceptFriendRequest(item.sid, item.rid, item.s_email, item.r_email)}}>Accepted</Button>
+                <Button onPress={() => {acceptFriendRequest(item.s_email, item.r_email)}}>Accept</Button>
               </View>
-            </View>   
+            </View>
           </TouchableOpacity>
         )}
       >
       </FlatList>
-      </View>
-    );
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    button: {
-      marginTop: 10,
-    },
-    container: {
-      flex: 1, 
-      paddingLeft: 20,
-      paddingRight: 20,
-      alignItems: 'center', 
-      justifyContent: 'center'
-    },
-    profile:{
-      width:'100%',
-    },
-    textSection:{
-      flexDirection: 'column',
-      justifyContent: 'center',
-      padding: 15,
-      paddingLeft: 0,
-      marginLeft: 10,
-      width: 399
-    },
-    userInfoText:{
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      marginBottom: 5,
-    },
-    userName:{
-      fontSize: 14,
-      fontWeight: 'bold',
-    },
-    postTime:{
-      fontSize: 12,
-      color: '#666',
-    },
-    messageText:{
-      fontSize: 14,
-      color: '#333333',
-    },
-    imgWrapper:{
-      paddingTop: 15,
-      paddingBottom: 15,
-    },
-    userImg:{
-      width: 50,
-      height: 50,
-      borderRadius: 25,
-    },
-    userInfo:{
-      flexDirection:'column',
-      justifyContent:'space-between',
-    },
-  });
+  button: {
+    marginTop: 10,
+  },
+  container: {
+    flex: 1, 
+    paddingLeft: 20,
+    paddingRight: 20,
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+  profile:{
+    width:'100%',
+  },
+  textSection:{
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: 15,
+    paddingLeft: 0,
+    marginLeft: 10,
+    width: 399
+  },
+  userInfoText:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 5,
+  },
+  userName:{
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  postTime:{
+    fontSize: 12,
+    color: '#666',
+  },
+  messageText:{
+    fontSize: 14,
+    color: '#333333',
+  },
+  imgWrapper:{
+    paddingTop: 15,
+    paddingBottom: 15,
+  },
+  userImg:{
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  userInfo:{
+    flexDirection:'column',
+    justifyContent:'space-between',
+  },
+});
