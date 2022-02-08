13f38d220b46cc49a3c92e27270c18da53d152ed
samsamho <67725786+samsamho@users.noreply.github.com>
2022-02-08 16:05:15 +0800

    Change button to lower case
    same as title

--- src/screens/SettingScreen.js
+++ src/screens/SettingScreen.js
@@ -9,16 +9,19 @@ export default function SettingScreen({ navigation }) {
         <View style={styles.container}>
             <Button
                 mode="contained"
+                uppercase={false}
                 style={styles.button}
                 onPress={() => navigation.navigate('Change Username')}
-            > Change username </Button>
+            > Change Username </Button>
             <Button
                 mode="contained"
+                uppercase={false}
                 style={styles.button}
                 onPress={() => navigation.navigate('VoiceEnroll')}
             > Enroll Voiceprint </Button>
             <Button
                 mode="contained"
+                uppercase={false}
                 style={styles.button}
                 contentStyle={styles.buttonContainer}
                 labelStyle={styles.navButtonText}
@@ -26,6 +29,7 @@ export default function SettingScreen({ navigation }) {
             >Change Profile Picture </Button>
             <Button
                 mode="contained"
+                uppercase={false}
                 style={styles.button}
                 onPress={async () => {
                     logout();
