eba78366297a6946f34684f38bd0e631a3c05fdf
PotatoHY <oscarmaths9141@gmail.com>
2022-04-03 18:27:08 +0800

    Fix touchable for username change

--- src/screens/SettingScreen.js
+++ src/screens/SettingScreen.js
@@ -9,150 +9,162 @@ import { AccountAuthContext } from '../contexts/AccountAuthContext'
 import { ThemeContext } from '../contexts/ThemeContext'
 
 export default function SettingScreen({ navigation }) {
-    const { user, logout } = useContext(AccountAuthContext)
-    const { theme } = useContext(ThemeContext)
-    const image = { uri: theme.background };
-    return(
-        <View style={styles.container}>
-            <ScrollView>
-            <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-                <View style={styles.profile(theme)}>
-                  <TouchableOpacity onPress={() => navigation.navigate('ChangeProfilePic')}>
-                    <Image
-                        style={styles.userImg}
-                        source={{
-                            uri:  theme.profile ,
-                        }}
-                    />
-                  </TouchableOpacity>
-                  <FontAwesome5 name="user-alt" size={theme.font} style={styles.icon(theme)} />
-                  <Text
-                    style={styles.username(theme)}
-                    onPress={() => {
-                      prompt("Edit username", "",
-                        [
-                          {text: 'Cancel'},
-                          {text: 'OK', onPress: async (newUserName) => {
-                              if (!newUserName) {
-                                Alert.alert("", "Please enter a username")
-                                return
-                              }
-                              const profile = {
-                                  displayName: newUserName,
-                                  photoURL: user.photoURL,
-                              }
-                              await user.updateProfile(profile)
-                              await firestore()
-                                  .collection('profiles')
-                                  .doc(user.email)
-                                  .update({
-                                      displayName: newUserName,
-                                  })
-                              Alert.alert("", "Username changed!")
-                          }},
-                        ],
-                        {
-                          type: 'plain-text',
-                          placeholder: 'Username'
-                        }
-                      );
-                    }}
-                  >{ theme.displayName }</Text>
-                  <EvilIcons name="pencil" size={theme.font * 1.5} color="#000000"style={styles.icon(theme)} />
-                </View>
-                <View style={styles.row(theme)}>
-                  <MaterialIcons name="email" size={theme.font * 1.5} style={styles.icon(theme)} />
-                  <Text
-                      style={styles.text(theme)}
-                  >{ user.email }</Text>
-                  <EvilIcons name="pencil" size={theme.font * 1.5} style={styles.icon(theme)} />
-                </View>
-                <View style={styles.row(theme)}>
-                  <FontAwesome5 name="user-friends" size={theme.font} style={styles.icon(theme)} />
-                  <Text
-                      uppercase={false}
-                      style={styles.text(theme)}
-                      onPress={async () => navigation.navigate('FriendRequest')}
-                  >Add friends</Text>
-                </View>
-                <View style={styles.row(theme)}>
-                  <MaterialIcons name="keyboard-voice" size={theme.font * 1.5} style={styles.icon(theme)} />
-                  <Text
-                      uppercase={false}
-                      style={styles.text(theme)}
-                      onPress={() => navigation.navigate('VoiceEnroll')}
-                  >Enroll Voiceprint </Text>
-                </View>
-                <View style={styles.row(theme)}>
-                  <Fontisto name="picture" size={theme.font} style={styles.icon(theme)} />
-                  <Text
-                      uppercase={false}
-                      style={styles.text(theme)}
-                      onPress={() => navigation.navigate('ChangeBackground')}
-                  >Change Background Picture</Text>
-                </View>
-                <View style={styles.row(theme)}>
-                  <Ionicons name="color-palette-outline" size={theme.font * 1.5} style={styles.icon(theme)} />
-                  <Text
-                      uppercase={false}
-                      style={styles.text(theme)}
-                      onPress={() => navigation.navigate('ChangeColor')}
-                  >Change Color Theme</Text>
-                </View>
-                <View style={styles.row(theme)}>
-                  <FontAwesome5 name="font" size={theme.font} style={styles.specialIcon(theme)} />
-                  <Text
-                      uppercase={false}
-                      style={styles.text(theme)}
-                      onPress={() => {
-                        prompt("Edit font size", "",
-                          [
-                            {text: 'Cancel'},
-                            {text: 'OK', onPress: async (newFontSize) => {
-                                if (!newFontSize || isNaN(newFontSize)) {
-                                  Alert.alert("", "Please enter a number")
-                                  return
-                                }
-                                await firestore()
-                                  .collection('profiles')
-                                  .doc(user.email)
-                                  .update({
-                                    fontSize: Number(newFontSize),
-                                  })
-                                Alert.alert("", "Font size changed!")
-                            }},
-                          ],
-                          {
-                            type: 'plain-text',
-                            placeholder: 'Font Size'
-                          }
-                      );
-                      }}
-                  >Change Font Size </Text>
-                </View>
-                <View style={styles.row(theme)}>
-                  <Ionicons name="log-out-outline" size={theme.font * 1.5} style={styles.redIcon(theme)} />
-                  <Text
-                      uppercase={false}
-                      style={styles.redText(theme)}
-                      onPress={async () => {
-                          logout();
-                      }}
-                  >Log out</Text>
-                </View>
-            </ImageBackground>
-        </ScrollView>
-        </View>
+  const { user, logout } = useContext(AccountAuthContext)
+  const { theme } = useContext(ThemeContext)
+  const image = { uri: theme.background };
+
+  const changeUsername = () => {
+    prompt("Edit username", "",
+      [
+        {text: 'Cancel'},
+        {text: 'OK', onPress: async (newUserName) => {
+            if (!newUserName) {
+              Alert.alert("", "Please enter a username")
+              return
+            }
+            const profile = {
+                displayName: newUserName,
+                photoURL: user.photoURL,
+            }
+            await user.updateProfile(profile)
+            await firestore()
+                .collection('profiles')
+                .doc(user.email)
+                .update({
+                    displayName: newUserName,
+                })
+            Alert.alert("", "Username changed!")
+        }},
+      ],
+      {
+        type: 'plain-text',
+        placeholder: 'Username'
+      }
     );
+  }
+
+  return(
+    <View style={styles.container}>
+      <ScrollView>
+        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+          <View style={styles.firstRow(theme)}>
+            <TouchableOpacity onPress={() => navigation.navigate('ChangeProfilePic')}>
+              <Image
+                style={styles.userImg}
+                source={{
+                  uri:  theme.profile ,
+                }}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => changeUsername()}>
+              <View style={styles.username(theme)} >
+                <FontAwesome5 name="user-alt" size={theme.font} style={styles.icon(theme)} />
+                <Text
+                  style={styles.usernameText(theme)}
+                >{ theme.displayName }</Text>
+                <EvilIcons name="pencil" size={theme.font * 1.5} color="#000000"style={styles.icon(theme)} />
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.row(theme)}>
+            <MaterialIcons name="email" size={theme.font * 1.5} style={styles.icon(theme)} />
+            <Text
+                style={styles.text(theme)}
+            >{ user.email }</Text>
+            <EvilIcons name="pencil" size={theme.font * 1.5} style={styles.icon(theme)} />
+          </View>
+          <View style={styles.row(theme)}>
+            <FontAwesome5 name="user-friends" size={theme.font} style={styles.icon(theme)} />
+            <Text
+                uppercase={false}
+                style={styles.text(theme)}
+                onPress={async () => navigation.navigate('FriendRequest')}
+            >Add friends</Text>
+          </View>
+          <View style={styles.row(theme)}>
+            <MaterialIcons name="keyboard-voice" size={theme.font * 1.5} style={styles.icon(theme)} />
+            <Text
+                uppercase={false}
+                style={styles.text(theme)}
+                onPress={() => navigation.navigate('VoiceEnroll')}
+            >Enroll Voiceprint </Text>
+          </View>
+          <View style={styles.row(theme)}>
+            <Fontisto name="picture" size={theme.font} style={styles.icon(theme)} />
+            <Text
+                uppercase={false}
+                style={styles.text(theme)}
+                onPress={() => navigation.navigate('ChangeBackground')}
+            >Change Background Picture</Text>
+          </View>
+          <View style={styles.row(theme)}>
+            <Ionicons name="color-palette-outline" size={theme.font * 1.5} style={styles.icon(theme)} />
+            <Text
+                uppercase={false}
+                style={styles.text(theme)}
+                onPress={() => navigation.navigate('ChangeColor')}
+            >Change Color Theme</Text>
+          </View>
+          <View style={styles.row(theme)}>
+            <FontAwesome5 name="font" size={theme.font} style={styles.specialIcon(theme)} />
+            <Text
+                uppercase={false}
+                style={styles.text(theme)}
+                onPress={() => {
+                  prompt("Edit font size", "",
+                    [
+                      {text: 'Cancel'},
+                      {text: 'OK', onPress: async (newFontSize) => {
+                          if (!newFontSize || isNaN(newFontSize)) {
+                            Alert.alert("", "Please enter a number")
+                            return
+                          }
+                          await firestore()
+                            .collection('profiles')
+                            .doc(user.email)
+                            .update({
+                              fontSize: Number(newFontSize),
+                            })
+                          Alert.alert("", "Font size changed!")
+                      }},
+                    ],
+                    {
+                      type: 'plain-text',
+                      placeholder: 'Font Size'
+                    }
+                );
+                }}
+            >Change Font Size </Text>
+          </View>
+          <View style={styles.row(theme)}>
+            <Ionicons name="log-out-outline" size={theme.font * 1.5} style={styles.redIcon(theme)} />
+            <Text
+                uppercase={false}
+                style={styles.redText(theme)}
+                onPress={async () => {
+                    logout();
+                }}
+            >Log out</Text>
+          </View>
+        </ImageBackground>
+      </ScrollView>
+    </View>
+  );
 }
 
 const { width, height } = Dimensions.get('screen');
 
 const styles = StyleSheet.create({
-  profile: (theme) => ({
+  firstRow: (theme) => ({
     flexDirection:'row',
     flexWrap:'wrap',
-    paddingBottom: theme.font / 2
+    alignItems: 'center',
+    paddingBottom: theme.font / 2,
+  }),
+  username: (theme) => ({
+    flexDirection:'row',
+    flexWrap:'wrap',
+    justifyContent: "center",
   }),
   row: (theme) => ({
     margin: theme.font * 2 / 3,
@@ -162,7 +174,7 @@ const styles = StyleSheet.create({
     borderBottomColor: 'lightgrey',
     borderBottomWidth: 1,
   }),
-  username: (theme) => ({
+  usernameText: (theme) => ({
     color: "black",
     fontSize: theme.font,
     textAlignVertical: "center",
