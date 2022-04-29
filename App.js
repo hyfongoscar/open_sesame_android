e6ac9532be72aaf9d2544bedd53982be556664b2
PotatoHY <oscarmaths9141@gmail.com>
2022-04-29 16:45:58 +0800

    Ignoring warnings

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import Providers from './src/navigation';
 
+import { LogBox } from 'react-native';
+LogBox.ignoreAllLogs(); //Ignore all log notifications
+
 export default function App() {
   return <Providers />;
 }
